ba189399305a2d789ce4594999a96c599427f25a
Pious79 <pierrelhermite@yahoo.fr>
2018-02-16 12:07:42 +0100

    English version + lowercase update

--- issp.R
+++ issp.R
@@ -1,41 +1,45 @@
-##______________________________________________________________________________##
-##  Function to calculate ISSP index of Pita                                    ##
-##  Pierre L'HERMITE - 2017-10-11 - fc.ISSP_PITA                                ##
-##______________________________________________________________________________##
-##------------------------------------------------------------------------------##
-#   Fonction : Calcul l'indice standardise de secheresses pluviometrique        ##
-#              de Pita avec la duree, l'intensite et le type de secheresse      ##
-##------------------------------------------------------------------------------##
-#   Arguments : data [zoo] : vecteur contenant les donnees mensuelles avec la
-#                            date au format %Y-%m-%d
-##------------------------------------------------------------------------------##
-#   Sortie    : ResISSP [list] : liste contenant 2 zoo et un df
-#                                (ISSP, Duree, Sech)
-#               ISSP [zoo] : vecteur contenant les valeurs de l'ISSP avec la
-#                            date au format %Y-%m-%d
-#               Duree [zoo] : vecteur contenant le nombre de mois cumule pour
-#                             chaque secheresse avec la date au format %Y-%m-%d
-#               Sech [df] : Dataframe contenant le nombre de secheresses selon 
-#                           leur type (ExtWet [ISSP>2], VeryWet [1.99>ISSP>1.5],
-#                           Wet [1.49>ISSP>1], Normal [0.99>ISSP>-0.99],
+##____________________________________________________________________________##
+##  Function to calculate ISSP index of Pita                                  ##
+##  Pierre L'HERMITE - 2017-10-11 - issp.R                                    ##
+##____________________________________________________________________________##
+##----------------------------------------------------------------------------##
+#   Function : Calcul the index about rainfall anomaly by month with the
+#              length, the drought type and the intensity
+##----------------------------------------------------------------------------##
+#   Input : monthly_data [zoo] : monthly data in zoo class with date in %Y-%m-%d
+##----------------------------------------------------------------------------##
+#   Output    : resissp [list] : list with 3 zoo et 1 dataframe
+#                                (issp, lengthzoo, drought_type, drought_number)
+#               issp [zoo] : zoo with the issp values with date in %Y-%m-%d
+#               lengthzoo [zoo] : zoo with the length of drought with date
+#                                 in %Y-%m-%d
+#               drought_type [zoo] :
+#               Dataframe contenant le nombre de secheresses selon 
+#                           leur type (Extwet [ISSP>2], Verywet [1.99>ISSP>1.5],
+#                           wet [1.49>ISSP>1], Normal [0.99>ISSP>-0.99],
 #                           Dry [-1>ISSP>-1.49], VeryDry [-1.5>ISSP>-1.99],
 #                           ExtDry [-2>ISSP])
-##------------------------------------------------------------------------------##
-#---------------------------------------------------------------------------------
+##----------------------------------------------------------------------------##
+#-------------------------------------------------------------------------------
 
-issp <- function(MonthlyData) {
+issp <- function(monthly_data) {
+  
   library(hydroTSM)
-  ## Verification arguments d'entree
-  if (!is.zoo(MonthlyData)) { stop("MonthlyData must be a zoo"); return(NULL) }
-  # --- Verification du pas de temps
-  if (sfreq(MonthlyData) != "monthly") {
-    stop("MonthlyData must be a daily serie \n"); return(NULL)
+  
+  ## Data input checking
+  if (!is.zoo(monthly_data)) { stop("monthly_data must be a zoo"); return(NULL)}
+  
+  # Times step checking
+  if (sfreq(monthly_data) != "monthly") {
+    stop("monthly_data must be a daily serie \n"); return(NULL)
   }
   
-  months <- substr(index(MonthlyData), 6, 7)
-  mediane <- aggregate(MonthlyData, by = months, FUN = median, na.rm = TRUE)
-  diff <- MonthlyData - coredata(mediane)[as.numeric(months)]
+  # Anomaly between precipitation and monthly median
+  months <- substr(index(monthly_data), 6, 7)
+  median_calc <- aggregate(monthly_data, by = months, FUN = median, na.rm = TRUE)
+  diff <- monthly_data - coredata(median_calc)[as.numeric(months)]
   
+  # 
   res <- rep(NA, length(diff))
   som <- 0
   
@@ -58,72 +62,78 @@ issp <- function(MonthlyData) {
     }
   }  
   
-  #calcul de l'indice de PITA, ISSP
-  Indice <- numeric()
+  # Index calcul, ISSP
+  vect_index <- numeric()
   
-  Mapa <- mean(res, na.rm = T)  
-  ECapa <- sd(res, na.rm = T)
+  mapa <- mean(res, na.rm = T)  
+  ecapa <- sd(res, na.rm = T)
   
-  Indice <- ((res-Mapa)/ECapa)
+  vect_index <- ((res-mapa) / ecapa)
   
-  ISSP <- zoo(as.numeric(Indice), index(MonthlyData))
+  issp <- zoo(as.numeric(vect_index), index(monthly_data))
   
-  #calcul la duree des secheresses
-  duree <- numeric()
+  # Calcul the length of drought
+  length_drought <- numeric()
+  
   n <- 0
   p <- 0
-  for (i in 1:length(ISSP)){
-    if (is.na(ISSP[i])){
-      duree[i] <- NA
-    } else if (ISSP[i] > 0){
+  
+  for (i in 1:length(issp)){
+    if (is.na(issp[i])){
+      length_drought[i] <- NA
+    } else if (issp[i] > 0){
       n <- 0
       p <- p + 1
-      duree[i] <- p
+      length_drought[i] <- p
     } else{
       p <- 0
       n <- n - 1 
-      duree[i] <- n
+      length_drought[i] <- n
     }
   }
   
-  dureezoo <- zoo(as.numeric(duree), index(MonthlyData))
+  lengthzoo <- zoo(as.numeric(length_drought), index(monthly_data))
   
-  #Classement secheresse
-  ExWet <- VWet <- Wet <- Normal <- Dry <- VDry <- ExDry <-0
-  Sech <- rep(NA, length(ISSP))
-  for(i in 1:length(ISSP)){
-    if( is.na(ISSP[i])){
+  # Drought type and number of drought
+  ext_wet <- very_wet <- wet <- normal <- dry <- very_dry <- ext_dry <-0
+  
+  drought_type <- rep(NA, length(issp))
+  
+  for(i in 1:length(issp)){
+    if( is.na(issp[i])){
     } 
-    else if((ISSP[i] >= 2)){
-      ExWet <- ExWet + 1
-      Sech[i] <- 3
-    } else if((1.99 > ISSP[i]) && (ISSP[i]> 1.5)){
-      VWet <- VWet + 1
-      Sech[i] <- 2
-    } else if((1.49 > ISSP[i]) && (ISSP[i]> 1)){
-      Wet <- Wet + 1
-      Sech[i] <- 1
-    } else if((0.99 > ISSP[i]) && (ISSP[i]> -0.99)){
-      Normal <- Normal + 1
-      Sech[i] <- 0
-    } else if((-1 >= ISSP[i]) && (ISSP[i]> -1.49)){
-      Dry <- Dry + 1
-      Sech[i] <- - 1
-    } else if((-1.5 >= ISSP[i]) && (ISSP[i]> -1.99)){
-      VDry <- VDry + 1
-      Sech[i] <- - 2
-    } else if((ISSP[i] <= -2)){
-      ExDry <- ExDry + 1
-      Sech[i] <- - 3
+    else if((issp[i] >= 2)){
+      ext_wet <- ext_wet + 1
+      drought_type[i] <- 3
+    } else if((1.99 > issp[i]) && (issp[i]> 1.5)){
+      very_wet <- very_wet + 1
+      drought_type[i] <- 2
+    } else if((1.49 > issp[i]) && (issp[i]> 1)){
+      wet <- wet + 1
+      drought_type[i] <- 1
+    } else if((0.99 > issp[i]) && (issp[i]> -0.99)){
+      normal <- normal + 1
+      drought_type[i] <- 0
+    } else if((-1 >= issp[i]) && (issp[i]> -1.49)){
+      dry <- dry + 1
+      drought_type[i] <- - 1
+    } else if((-1.5 >= issp[i]) && (issp[i]> -1.99)){
+      very_dry <- very_dry + 1
+      drought_type[i] <- - 2
+    } else if((issp[i] <= -2)){
+      ext_dry <- ext_dry + 1
+      drought_type[i] <- - 3
     } else {}
   }
   
-  nombre <- rbind.data.frame(ExWet, VWet, Wet, Normal, Dry, VDry, ExDry)
-  colnames(nombre) <- c("Pluvio")
-  row.names(nombre) <- c("ExWet", "VWet", "Wet", "Normal", "Dry", "VDry",
-                         "ExDry")
+  drought_number <- rbind.data.frame(ext_wet, very_wet, wet, normal,
+                                     dry, very_dry, ext_dry)
+  colnames(drought_number) <- c("Pluvio")
+  row.names(drought_number) <- c("ExWet", "VWet", "Wet", "Normal", "Dry",
+                                 "VDry", "ExDry")
   
-ResISSP <- list(ISSP = ISSP, Duree = dureezoo, Drought = nombre, SechTime = Sech)
-return(ResISSP)
+resissp <- list(issp = issp, length = lengthzoo, drought = drought_number,
+                drought_time = drought_type)
+return(resissp)
 
 }
