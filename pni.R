a2caf1fd48ea766d18baa037908310d7c831ffdd
Pious79 <pierrelhermite@yahoo.fr>
2018-03-01 20:00:54 +0100

    English version

--- pni.R
+++ pni.R
@@ -3,95 +3,117 @@
 ##  Pierre L'HERMITE - 2017-10-17 - fc.PNI.R                                  ##
 ##____________________________________________________________________________##
 ##----------------------------------------------------------------------------##
-#   Fonction : Calcul du PNI                                                  ##
+#   Description: Calculation of percent of normal index (PNI)                 ##
 ##----------------------------------------------------------------------------##
-#   Arguments : MonthlyData [zoo] : vecteur contenant les donnees mensuelles 
-#                                   avec la date au format %Y-%m-%d
-#               Delta [numeric] : defaut = 12, période pour le cumul des 
-#                                 précipitations mensuelles (1 à 48)
+#   Arguments: monthly_data [zoo] : rainfall monthly data in zoo class 
+#                                   with date in %Y-%m-%d
+#              time_step [numeric] : default = 12, time step to sum monthly 
+#                                   precipitation (1, 3, 6, 9, 12, 24 and 48)
 ##----------------------------------------------------------------------------##
-#   Sortie    : ResPNI [list] : liste contenant :
-#               PNI [zoo] : vecteur contenant les valeurs du PNI
-#               Nombre [dataframe] : tableau recapitulant les types de periodes
-#                                   seches et humides
+#   Values: respni [list] : list with 3 zoo et 1 dataframe
+#                           (pni, length_zoo, drought_type, drought_number)
+#           pni [zoo] : zoo with the pni values with date in %Y-%m-%d
+#           length_zoo [zoo] : zoo with the length of drought with date
+#                              in %Y-%m-%d
+#           drought_type [zoo] : zoo with the type of the period for
+#                                 each month 
+#           drought_number [dataframe] : dataframe with the number of 
+#                           different period by type
+#                           Extwet [issp>2], Verywet [1.99>issp>1.5],
+#                           wet [1.49>issp>1], Normal [0.99>issp>-0.99],
+#                           Dry [-1>issp>-1.49], VeryDry [-1.5>issp>-1.99],
+#                           ExtDry [-2>issp])
 ##----------------------------------------------------------------------------##
 #-------------------------------------------------------------------------------
 
-pni <- function(MonthlyData, Delta = 12){
-  library(hydroTSM)
-  ## Verification arguments d'entree
-  if (!is.zoo(MonthlyData)) { stop("MonthlyData must be a zoo"); return(NULL) }
-  # --- Verification du pas de temps
-  if (sfreq(MonthlyData) != "monthly") {
-    stop("MonthlyData must be a daily serie \n"); return(NULL)
+pni <- function(monthly_data, time_step = 12){
+  
+  ##__Checking______________________________________________________________####
+  # Data input checking
+  if (!is.zoo(monthly_data)) { stop("monthly_data must be a zoo"); return(NULL)}
+  
+  # Time step checking
+  if (periodicity(monthly_data)$scale != "monthly") {
+    stop("monthly_data must be a monthly serie \n"); return(NULL)
   }
   
-  #Somme selon le pas de temps
-  tmp_mean <- as.data.frame(matrix(NA, ncol = Delta, nrow = length(MonthlyData)))
-  for(i in 1:Delta) {
-    tmp_mean[, i] <- coredata(MonthlyData)[i:(length(MonthlyData)+i-1)]
+  ##__Calculation___________________________________________________________####
+  # Sum with time step
+  tmp_mean <- as.data.frame(matrix(NA, ncol = time_step,
+                                   nrow = length(monthly_data)))
+  
+  for(i in 1:time_step) {
+    tmp_mean[, i] <- coredata(monthly_data)[i:(length(monthly_data) + i - 1)]
   }
-  somme <- apply(tmp_mean, 1, sum, na.rm = FALSE)
-  moy<-mean(somme, na.rm = TRUE)
-  cumul <- zoo(somme, order.by = index(MonthlyData))
   
-  #Calcul de l'indice PNI
-  PNI <- (cumul/moy)*100
+  sum <- apply(tmp_mean, 1, sum, na.rm = FALSE)
+  sum_mean <- mean(sum, na.rm = TRUE)
+  sum_zoo <- zoo(sum, order.by = index(monthly_data))
   
-  #Caracterisation des periodes
-  ExWet <- VWet <- Wet <- Normal <- Dry <- VDry <- ExDry <- 0
-  Sech <- rep(NA, length(PNI))
-  for(iLength in 1:length(PNI)){
-    if( is.na(PNI[iLength])){
-    } else if (PNI[iLength] >= 160){
-      ExWet <- ExWet + 1
-      Sech[iLength] <- 3
-    } else if ((PNI[iLength] >= 145 && (PNI[iLength] < 160))) {
-      VWet <- VWet + 1
-      Sech[iLength] <- 2
-    } else if ((PNI[iLength] > 120 && (PNI[iLength] < 145))) {
-      Wet <- Wet + 1
-      Sech[iLength] <- 1
-    } else if((120 >= PNI[iLength]) && (PNI[iLength] > 80)){
-      Normal <- Normal + 1
-      Sech[iLength] <- 0
-    } else if((80 >= PNI[iLength]) && (PNI[iLength] > 55)){
-      Dry <- Dry + 1
-      Sech[iLength] <- - 1
-    } else if((55 >= PNI[iLength]) && (PNI[iLength] > 40)){
-      VDry <- VDry + 1
-      Sech[iLength] <- - 2
-    } else if((PNI[iLength] <= 40)){
-      ExDry <- ExDry + 1
-      Sech[iLength] <- - 3
+  # Calculation of PNI
+  pni <- (sum_zoo/sum_mean)*100
+  
+  ##__Index analysis________________________________________________________####
+  # Drought type and number of drought
+  ext_wet <- very_wet <- wet <- normal <- dry <- very_dry <- ext_dry <- 0
+  
+  drought_type <- rep(NA, length(pni))
+  
+  for(ilength in 1:length(pni)){
+    if( is.na(pni[ilength])){
+    } else if (pni[ilength] >= 160){
+      ext_wet <- ext_wet + 1
+      drought_type[ilength] <- 3
+    } else if ((pni[ilength] >= 145 && (pni[ilength] < 160))) {
+      very_wet <- very_wet + 1
+      drought_type[ilength] <- 2
+    } else if ((pni[ilength] > 120 && (pni[ilength] < 145))) {
+      wet <- wet + 1
+      drought_type[ilength] <- 1
+    } else if((120 >= pni[ilength]) && (pni[ilength] > 80)){
+      normal <- normal + 1
+      drought_type[ilength] <- 0
+    } else if((80 >= pni[ilength]) && (pni[ilength] > 55)){
+      dry <- dry + 1
+      drought_type[ilength] <- - 1
+    } else if((55 >= pni[ilength]) && (pni[ilength] > 40)){
+      very_dry <- very_dry + 1
+      drought_type[ilength] <- - 2
+    } else if((pni[ilength] <= 40)){
+      ext_dry <- ext_dry + 1
+      drought_type[ilength] <- - 3
     } else {}
   }
   
-  nombre <- rbind.data.frame(ExWet, VWet, Wet, Normal, Dry, VDry, ExDry)
-  colnames(nombre) <- c("Pluvio")
-  row.names(nombre) <- c("Extreme Wet", "Very Wet", "Wet", "Normal", "Dry",
-                       "Very Dry", "Extreme Dry")
+  drought_number <- rbind.data.frame(ext_wet, very_wet, wet, normal, dry, very_dry,
+                             ext_dry)
+  colnames(drought_number) <- c("Rain gauge")
+  row.names(drought_number) <- c("Extreme Wet", "Very Wet", "Wet", "Normal", "Dry",
+                         "Very Dry", "Extreme Dry")
   
-  #calcul la duree des secheresses
-  duree <- numeric()
-  neg <- 0
-  pos <- 0
-  for (iLength in 1:length(PNI)) {
-    if (is.na(PNI[iLength])){
-      duree[iLength] <- NA
-    } else if (PNI[iLength] > 100) {
-      neg <- 0
-      pos <- pos + 1
-      duree[iLength] <- pos
+  # Calculate the length of drought
+  length_drought <- numeric()
+  
+  n <- 0
+  p <- 0
+  
+  for (ilength in 1:length(pni)) {
+    if (is.na(pni[ilength])){
+      duree[ilength] <- NA
+    } else if (pni[ilength] > 100) {
+      n <- 0
+      p <- p + 1
+      duree[ilength] <- p
     } else {
-      pos <- 0
-      neg <- neg - 1 
-      duree[iLength] <- neg
+      p <- 0
+      n <- n - 1 
+      duree[ilength] <- n
     }
   }
   
-  dureezoo <- zoo(as.numeric(duree), index(MonthlyData))
+  length_zoo <- zoo(as.numeric(duree), index(monthly_data))
   
-  ResPNI <- list(PNI = PNI, Drought = nombre, Duree = dureezoo, SechTime = Sech)
-  return(ResPNI)
+  respni <- list(pni = pni, drougth_length = length_zoo,
+                 drought_number_type = drought_number, type_time = drought_type)
+  return(Respni)
 }
