ea3f3cc43159ad12686e6f9fd6f81a57db152fb1
Pious79 <pierrelhermite@yahoo.fr>
2018-03-06 20:33:06 +0100

    Starting english version

--- spi.R
+++ spi.R
@@ -1,55 +1,53 @@
 ##____________________________________________________________________________##
-##  Function to calculate SPI index                                           ##
-##  Pierre L'HERMITE - 2017-10-12 - fc.SPI.R                                  ##
+##  Function to calculate SPI                                                 ##
+##  Pierre L'HERMITE - 2017-10-12 - spi.R                                     ##
 ##____________________________________________________________________________##
 ##----------------------------------------------------------------------------##
-#   Fonction : Calcul l'indice standardise des precipitations avec les types  ##
-#              de secheresse et la duree                                      ##
+#   Description: Calculate the standardized precipitation index and the       ##
+#                drought specifications                                       ##
 ##----------------------------------------------------------------------------##
-#   Arguments : MonthlyData [zoo] : vecteur contenant les donnees mensuelles 
-#                                   des precipitations avec la date au format 
-#                                   %Y-%m-%d [mm/month]
-#               Delta [numeric] : valeur du pas de temps pour le SPI (1, 3, 6, 9
-#                                 ,12, 24, et 48 en general) [en nombre de mois]
-#               Distribution [character] : distribution des donnees (gamma,
-#                                          grev, genlog, normal)
+#   Arguments: prec_data [zoo]: rainfall monthly data in zoo class 
+#                               with date in %Y-%m-%d
+#              time_step [numeric] : default = 12, time step to sum monthly 
+#                                   precipitation (1, 3, 6, 9, 12, 24 and 48)
+#              distribution [character] : distribution of data (log_Logistic,
+#                                           gamma, grev, genlog, normal)
 ##----------------------------------------------------------------------------##
-#   Sortie    : ResSPI [list] : liste contenant 2 zoo et un data frame
-#                               (SPI , Drought, Time)
-#               SPI [zoo] : vecteur contenant les valeurs du SPI avec la
-#                           date au format %Y-%m-%d
-#               Drought [df] : recapitulatif du nombre de secheresse par les
-#                             differentes categories de l'SPI (ExtWet [SPI>2],
-#                             VeryWet [1.99>SPI>1.5], Wet [1.49>SPI>1], Normal
-#                             [0.99>SPI>-0.99], Dry [-1>SPI>-1.49], VeryDry
-#                             [-1.5>SPI>-1.99], ExtDry [-2>SPI])
-#               Time [zoo] : vecteur contenant la duree des differentes periodes
-#                            avec la date au format %Y-%m-%d. Lorsque l'indice
-#                            positif, c'est une periode humide et lorsque
-#                            l'indice est negatif, la periode est seche
+#   Values: resspi [list] : list with 3 zoo et 1 dataframe
+#                           (spi, length_zoo, drought_type, drought_number)
+#           spi [zoo] : zoo with the spi values with date in %Y-%m-%d
+#           length_zoo [zoo] : zoo with the length of drought with date
+#                              in %Y-%m-%d
+#           drought_type [zoo] : zoo with the type of the period for
+#                                 each month 
+#           drought_number [dataframe] : dataframe with the number of 
+#                           different period by type
+#                           Extwet [spi>2], Verywet [1.99>spi>1.5],
+#                           wet [1.49>spi>1], Normal [0.99>spi>-0.99],
+#                           Dry [-1>spi>-1.49], VeryDry [-1.5>spi>-1.99],
+#                           ExtDry [-2>spi])
 ##----------------------------------------------------------------------------##
 #-------------------------------------------------------------------------------
 
-spi <- function(MonthlyData, Delta=12, Distribution = "gamma"){
+spi <- function(prec_data, time_step = 12, distribution = "gamma"){
   
-  library(hydroTSM)
   library(SCI)
   ## Verification arguments d'entree
-  if (!is.zoo(MonthlyData)) { stop("MonthlyData must be a zoo"); return(NULL) }
+  if (!is.zoo(prec_data)) { stop("prec_data must be a zoo"); return(NULL) }
   # --- Verification du pas de temps
-  if (sfreq(MonthlyData) != "monthly") {
-    stop("MonthlyData must be a daily serie \n"); return(NULL)
+  if (sfreq(prec_data) != "monthly") {
+    stop("prec_data must be a daily serie \n"); return(NULL)
   }
   
   #Premier mois
-  firstmonth <- as.numeric(substr(index(MonthlyData[1]), 6, 7))
+  firstmonth <- as.numeric(substr(index(prec_data[1]), 6, 7))
   
   #Utilisation du package SCI pour calculer le SPI et creation zoo
-  spi.para <- fitSCI(coredata(MonthlyData), first.mon = firstmonth,
-                     distr = Distribution, time.scale = Delta, p0 = TRUE)
-  SPI_val <- transformSCI(coredata(MonthlyData), first.mon = firstmonth,
+  spi.para <- fitSCI(coredata(prec_data), first.mon = firstmonth,
+                     distr = Distribution, time.scale = time_step, p0 = TRUE)
+  SPI_val <- transformSCI(coredata(prec_data), first.mon = firstmonth,
                       obj = spi.para)
-  SPI <- zoo(SPI_val, order.by = index(MonthlyData))
+  SPI <- zoo(SPI_val, order.by = index(prec_data))
   
   #Comptage du nombre de secheresse
   ExWet <- VWet <- Wet <- Normal <- Dry <- VDry <- ExDry<-0
